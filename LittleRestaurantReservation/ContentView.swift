0db72a56f7924a7cd5f5cda07ab8fde01ecc9a8c
Moises Gil <moisesgilsantana@gmail.com>
2025-12-13 21:36:03 -0400

    Initial Commit

---
+++ LittleRestaurantReservation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LittleRestaurantReservation
+//
+//  Created by Moises Gil on 12/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LittleRestaurantReservation/LittleRestaurantReservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleRestaurantReservationApp.swift
+//  LittleRestaurantReservation
+//
+//  Created by Moises Gil on 12/13/25.
+//
+
+import SwiftUI
+
+@main
+struct LittleRestaurantReservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
